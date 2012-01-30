d0dcf8626a9878fa5e23d3d5eb3803cfdf5fb9b1
Rob Masters <mastahuk@gmail.com>
2012-01-30 19:48:54 +0000

    bootsrap experimentation...

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,24 +3,16 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 if (file_exists($file = __DIR__ . '/../vendor/.composer/autoload.php')) {
     $autoload = require_once $file;
-} else {
+} elseif(isset($_SERVER['SYMFONY'])) {
     require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 
     $loader = new UniversalClassLoader();
     $loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
     $loader->register();
-}
-
-
-
-/*
-if (file_exists($file = __DIR__ . '/../vendor/.composer/autoload.php')) {
-    $autoload = require_once $file;
 } else {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
-*/
 
 spl_autoload_register(function($class) {
     $class = ltrim($class, '\\');
