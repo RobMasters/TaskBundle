4a4483640b410fddcd19b938534e0a90ed7daa7e
Rob Masters <mastahuk@gmail.com>
2012-01-30 19:45:23 +0000

    bootsrap experimentation...

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,12 +1,26 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+if (file_exists($file = __DIR__ . '/../vendor/.composer/autoload.php')) {
+    $autoload = require_once $file;
+} else {
+    require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
+    use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->register();
+    $loader = new UniversalClassLoader();
+    $loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
+    $loader->register();
+}
+
+
+
+/*
+if (file_exists($file = __DIR__ . '/../vendor/.composer/autoload.php')) {
+    $autoload = require_once $file;
+} else {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+*/
 
 spl_autoload_register(function($class) {
     $class = ltrim($class, '\\');
